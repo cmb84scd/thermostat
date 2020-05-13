25e3772913482618c374c5812de1776360404af8
Joanne Young <joanneyoung_@hotmail.com>
2020-05-13 17:56:29 +0100

    thermostat has max limits depending on PSM

--- src/thermostat.js
+++ src/thermostat.js
@@ -6,6 +6,8 @@ class Thermostat {
     this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
     this.isPowerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
   };
 
   getCurrentTemperature(){
@@ -13,6 +15,9 @@ class Thermostat {
   }
 
   up(){
+    if (this.isMaximumTemperature()){
+      return;
+    }
     this.temperature += 1;
   }
 
@@ -38,4 +43,12 @@ class Thermostat {
   switchPowerSavingModeOn(){
     this.isPowerSavingMode = true;
   }
+
+  isMaximumTemperature(){
+    if(this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+
 };
